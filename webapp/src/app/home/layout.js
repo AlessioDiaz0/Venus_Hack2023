aac8eae69bea956cf15f20c012c4110242c5c8cd
Linda Vo <volinda2000@gmail.com>
2023-05-27 12:32:50 -0700

    add home and dashboard pages

---
+++ webapp/src/app/home/layout.js
@@ -0,0 +1,12 @@
+export default function HomeLayout({ children }) {
+  return (
+    <main class="min-h-screen bg-gradient-to-b from-black via-slate-950 to-blue-950">
+      <div class="flex justify-center">
+        <span class="font-Nunito font-extrabold text-4xl bg-gradient-to-r from-fuchsia-200 to-violet-400 bg-clip-text text-transparent">
+          VenusLingo!
+        </span>
+      </div>
+      {children}
+    </main>
+  );
+}
