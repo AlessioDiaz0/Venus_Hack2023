39607da9b346afb80f9039edb5d996aa4eeacf4e
Alessio Diaz Cama <adiazcam@uci.edu>
2023-05-26 21:42:45 -0700

    updated commit

--- translator_api.js
+++ translator_api.js
@@ -1,12 +1,13 @@
-async function translateText() {
+//word_selected: is the word to be translated to the base_language
+//base_language: is the default language set on the user google account
+async function translateText(word_selected, base_language) {
 	const res = await fetch("https://libretranslate.com/translate", {
 		method: "POST",
 		body: JSON.stringify({
-			q: "",
+			q: "word_selected",
 			source: "auto",
-			target: "en", //Change 'en' to a variable (language selected by user)
+			target: "base_language",
 			format: "text",
-			api_key: ""
 		}),
 		headers: { "Content-Type": "application/json" }
 	});
@@ -15,3 +16,7 @@ async function translateText() {
 	//console.log(data); print out the out come
 	return data;
 }
+
+translateText().then(data => {
+	console.log(data);
+  });
